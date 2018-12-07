b93141d34782186d6ff45247d04f49e75beb4342
Wan Yi <mail@wanyi.me>
2018-12-07 15:25:47 +0800

    minor change

--- action-eslint/entrypoint.sh
+++ action-eslint/entrypoint.sh
@@ -7,7 +7,7 @@ set -eu
 mkdir -p /secrets
 gpg -q --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" -o /secrets/docker_id_rsa docker_id_rsa.gpg
 chmod 400 /secrets/docker_id_rsa
-ssh-keygen -R github.com
+# ssh-keygen -R github.com
 
 # checkout submodules
 ssh-agent bash -c 'ssh-add /secrets/docker_id_rsa; git submodule sync; git submodule update --init;'
