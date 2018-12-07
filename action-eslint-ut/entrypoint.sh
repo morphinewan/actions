5470514df47caacceca94da251293d2309666219
Wan Yi <mail@wanyi.me>
2018-12-07 15:09:48 +0800

    私钥权限修正

--- action-eslint-ut/entrypoint.sh
+++ action-eslint-ut/entrypoint.sh
@@ -6,6 +6,7 @@ set -eu
 # Decrypt the file
 mkdir -p /secrets
 gpg -q --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" -o /secrets/docker_id_rsa docker_id_rsa.gpg
+chmod 400 /secrets/docker_id_rsa
 
 # checkout submodules
 ssh-agent bash -c 'ssh-add /secrets/docker_id_rsa; git submodule sync; git submodule update --init;'
