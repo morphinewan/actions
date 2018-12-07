8bed482ad54ec42ab4db444a7983eab19fce183b
Wan Yi <mail@wanyi.me>
2018-12-07 14:54:56 +0800

    修改脚本适配gpg密钥

--- action-eslint-ut/entrypoint.sh
+++ action-eslint-ut/entrypoint.sh
@@ -3,15 +3,26 @@
 # avoid continuing when errors or undefined variables are present.
 set -eu
 
-# checkout submodules
-git submodule sync
-git submodule update --init
+# Decrypt the file
+mkdir -p /secrets
+gpg -q --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" -o /secrets/docker_id_rsa docker_id_rsa.gpg
 
-# 安装环境依赖
+# checkout submodules
+ssh-agent bash -c 'ssh-add /secrets/docker_id_rsa; git submodule sync; git submodule update --init;'
+
+# Delete Secrets
+rm -rf /secrets
+
+# checkout submodules#
+# git submodule sync
+# git submodule update --init
+
+# 安装环境依赖#
 yarn install
 
-# 测试eslint
-yarn lint-ci
-
-# 单体测试
-yarn test --ci --testResultsProcessor="jest-junit"
+#
+# # 测试eslint
+# yarn lint-ci
+#
+# # 单体测试
+# yarn test --ci --testResultsProcessor="jest-junit"
