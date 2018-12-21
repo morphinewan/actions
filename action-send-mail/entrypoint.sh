35cf42b549c0f2630e4357d8477d3eb350228f09
Wan Yi <mail@wanyi.me>
2018-12-21 14:43:01 +0800

    fix email shell

--- action-send-mail/entrypoint.sh
+++ action-send-mail/entrypoint.sh
@@ -6,5 +6,5 @@ curl -s --user 'api:$MAIL_API_KEY' \
     https://api.mailgun.net/v3/$MAIL_DOMAIN/messages \
     -F from='WebMaster <webmaster@$MAIL_DOMAIN>' \
     -F to=$MAIL_TO \
-    -F subject=$MAIL_SUBJECT \
-    -F text=$MAIL_CONTENT
+    -F subject='$MAIL_SUBJECT' \
+    -F text='$MAIL_CONTENT'
