6aa1e699a98153971cd4dca0445c0271bdb42dbb
Wan Yi <mail@wanyi.me>
2018-12-21 14:40:42 +0800

    Update entrypoint.sh

--- action-send-mail/entrypoint.sh
+++ action-send-mail/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 
 curl -s --user 'api:$MAIL_API_KEY' \
     https://api.mailgun.net/v3/$MAIL_DOMAIN/messages \
-    -F from='WebMaster <webmaster@$MAILGUN_DOMAIN>' \
+    -F from='WebMaster <webmaster@$MAIL_DOMAIN>' \
     -F to=$MAIL_TO \
     -F subject=$MAIL_SUBJECT \
     -F text=$MAIL_CONTENT
