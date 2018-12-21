b6a7fe353c9112a36a7a3177a4f7f012c1da112c
Wan Yi <mail@wanyi.me>
2018-12-21 12:09:49 +0800

    增加发邮件action

---
+++ action-send-mail/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+curl -s --user 'api:$MAIL_API_KEY' \
+    https://api.mailgun.net/v3/$MAIL_DOMAIN/messages \
+    -F from='WebMaster <webmaster@$MAILGUN_DOMAIN>' \
+    -F to=$MAIL_TO \
+    -F subject=$MAIL_SUBJECT \
+    -F text=$MAIL_CONTENT
