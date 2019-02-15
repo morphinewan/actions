1276e542d3fe955772da65f4d68467430732bfcd
Wan Yi <mail@wanyi.me>
2019-02-15 18:38:14 +0800

    将release tag作为sentry release

--- action-sentry-release/entrypoint.sh
+++ action-sentry-release/entrypoint.sh
@@ -8,13 +8,15 @@ export PATH=$(npm bin):$PATH
 # $SENTRY_AUTH_TOKEN
 # $SENTRY_ORG
 
+TAG=$(./tag $GITHUB_EVENT_PATH)
+
 SENTRY_VERSION=$(sentry-cli releases propose-version)
 
 # Create a release
-sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_VERSION
+sentry-cli releases new --finalize -p $SENTRY_PROJECT $TAG
 
 # Associate commits with the release
 sentry-cli releases set-commits --auto $SENTRY_VERSION
 
 # Tell Sentry When You Deploy a Release
-sentry-cli releases deploys VERSION new -e ENVIRONMENT
+sentry-cli releases deploys $TAG new
