300c992a9bb5ba4def42d518b6f23b14344a00da
Wan Yi <mail@wanyi.me>
2019-02-17 07:42:36 +0800

    改善sentry脚本功能

--- action-sentry-release/entrypoint.sh
+++ action-sentry-release/entrypoint.sh
@@ -14,16 +14,16 @@ echo "Create a release $TAG"
 sentry-cli releases new --finalize -p $SENTRY_PROJECT $TAG
 
 echo "Associate commits $SENTRY_VERSION with the release"
-sentry-cli releases set-commits --auto $SENTRY_VERSION
+sentry-cli releases set-commits "$TAG" --commit "${GITHUB_REPOSITORY}@${SENTRY_VERSION}"
 
 # Upload Source Maps
 for file in ./dist/static/js/*/*.js.map
 do
     if test -f $file
     then
-        URL_PREFIX=$(echo $file | awk -F / '{print "~/js/"$(NF-1)"/"$NF}')
+        URL_PREFIX=$(echo $file | awk -F / '{print "~/js/"$(NF-1)}')
         echo "Upload sourcemap $file with url-prefix ${URL_PREFIX}"
-        sentry-cli releases files $TAG upload-sourcemaps $file --url-prefix '${URL_PREFIX}'
+        sentry-cli releases files $TAG upload-sourcemaps $file --url-prefix ${URL_PREFIX}
     fi
 done
 
