2dc8f385276acafd1ad3054c62a93bdd178fb185
Wan Yi <mail@wanyi.me>
2019-02-16 18:19:46 +0800

    加入上传SourceMap的功能

--- action-sentry-release/entrypoint.sh
+++ action-sentry-release/entrypoint.sh
@@ -10,11 +10,20 @@ TAG=$(./tag $GITHUB_EVENT_PATH)
 
 SENTRY_VERSION=$(sentry-cli releases propose-version)
 
-# Create a release
+echo "Create a release $TAG"
 sentry-cli releases new --finalize -p $SENTRY_PROJECT $TAG
 
-# Associate commits with the release
+echo "Associate commits $SENTRY_VERSION with the release"
 sentry-cli releases set-commits --auto $SENTRY_VERSION
 
-# Tell Sentry When You Deploy a Release
+# Upload Source Maps
+for file in ./dist/static/js/*/*.js.map
+    if test -f $file
+    then
+        echo "Upload sourcemap $file"
+        sentry-cli releases files $TAG upload-sourcemaps $file
+    fi
+fi
+
+echo "Deploy a Release $TAG"
 sentry-cli releases deploys $TAG new -e PRODUCTION
