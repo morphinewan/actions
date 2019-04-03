4a1c65ae5a0162fc8455a0cbddb72dfb80d3fd75
Wan Yi <root@wanyi.dev>
2019-04-03 19:08:54 +0800

    fix flutter action

--- action-flutter-test/entrypoint.sh
+++ action-flutter-test/entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -eu
 
-if [ -d $HOME/.flutter ]
-then
-    mkdir -p $HOME/.flutter
-fi
+# if [ -d $HOME/.flutter ]
+# then
+#     mkdir -p $HOME/.flutter
+# fi
+
+ls -lah /home/morphine
+
+ln -s $HOME/.flutter /home/morphine/.flutter
 
 flutter test test/
