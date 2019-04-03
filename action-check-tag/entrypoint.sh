8660a8700d09cd37d3b7ff586c2372a839a4ca1d
Wan Yi <root@wanyi.dev>
2019-04-03 17:08:45 +0800

    remove action-check-tag

--- action-check-tag/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-cp /tag ./tag
-TAG=$(./tag $GITHUB_EVENT_PATH)
-echo "========================================"
-echo Tag: $TAG
-echo "========================================"
