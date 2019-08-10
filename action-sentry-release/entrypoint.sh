22e5ca76079bb323604ce717707e6fa9a383f61c
Wan Yi <root@wanyi.dev>
2019-08-10 08:47:07 +0800

    Update entrypoint.sh

--- action-sentry-release/entrypoint.sh
+++ action-sentry-release/entrypoint.sh
@@ -22,14 +22,4 @@ sentry-cli releases new --finalize -p $SENTRY_PROJECT $TAG
 echo "Associate commits $GITHUB_SHA with the release $TAG"
 sentry-cli releases set-commits "$TAG" --commit "${GITHUB_REPOSITORY}@${GITHUB_SHA}"
 
-# Upload Source Maps
-for file in ./dist/static/js/*/*.js.map
-do
-    if test -f $file
-    then
-        URL_PREFIX=$(echo $file | awk -F / '{print "~/js/"$(NF-1)}')
-        sentry-cli releases files $TAG upload-sourcemaps $file --url-prefix ${URL_PREFIX} --rewrite
-    fi
-done
-
 sentry-cli releases deploys $TAG new -e $SENTRY_ENVIRONMENT
